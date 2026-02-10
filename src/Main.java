24d59ecdc1f5934ef6a72993593fddfff9e63888
Karwan Ali <karwana0000@gmail.com>
2026-02-10 23:27:17 +0100

    class for connection to database + Customer class + started Main class.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import models.Customer;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static Customer inloggadKund = null;
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
---
+++ src/models/Customer.java
@@ -0,0 +1,87 @@
+package models;
+
+public class Customer {
+    private int kundId;
+    private String förnamn;
+    private String efternamn;
+    private String adress;
+    private String postnummer;
+    private String ort;
+    private String lösenord;
+
+    public Customer() {
+    }
+
+    public Customer(int kundId, String förnamn, String efternamn, String adress,
+                    String postnummer, String ort, String lösenord) {
+        this.kundId = kundId;
+        this.förnamn = förnamn;
+        this.efternamn = efternamn;
+        this.adress = adress;
+        this.postnummer = postnummer;
+        this.ort = ort;
+        this.lösenord = lösenord;
+    }
+
+
+    public int getKundId() {
+        return kundId;
+    }
+
+    public void setKundId(int kundId) {
+        this.kundId = kundId;
+    }
+
+    public String getFörnamn() {
+        return förnamn;
+    }
+
+    public void setFörnamn(String förnamn) {
+        this.förnamn = förnamn;
+    }
+
+    public String getEfternamn() {
+        return efternamn;
+    }
+
+    public void setEfternamn(String efternamn) {
+        this.efternamn = efternamn;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPostnummer() {
+        return postnummer;
+    }
+
+    public void setPostnummer(String postnummer) {
+        this.postnummer = postnummer;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+
+    public String getLösenord() {
+        return lösenord;
+    }
+
+    public void setLösenord(String lösenord) {
+        this.lösenord = lösenord;
+    }
+
+    @Override
+    public String toString() {
+        return förnamn + " " + efternamn + " (" + ort + ")";
+    }
+}
